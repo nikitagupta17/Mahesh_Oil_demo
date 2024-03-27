892bc24e8d2acd0b7db1ffde80a8cb812be6fdfb
Nikita Gupta <61657681+nikitagupta17@users.noreply.github.com>
2024-03-27 14:05:12 +0530

    Update sales_app.py

--- sales_app.py
+++ sales_app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Load the dataset
 @st.cache(allow_output_mutation=True)
 def load_data():
-    df = pd.read_excel("sales_data_v6.xlsx")
+    df = pd.read_csv("sales_data_v6.csv")
     df["Expected Order Date"] = pd.to_datetime(df["Expected Order Date"]).dt.date  # Convert to date only
     return df
 
